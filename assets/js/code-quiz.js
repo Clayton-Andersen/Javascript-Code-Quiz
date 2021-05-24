d2f6dde4277dafee59597909639884ca912a275d
Aloha <utah.clayton@gmail.com>
2021-05-23 22:43:51 -0700

    typed out questions and organized array to loop through

--- assets/js/code-quiz.js
+++ assets/js/code-quiz.js
@@ -1,10 +1,4 @@
-// Questions
 
-// Inside which html tag do we put the JavaScript?
-// 1. <scripting>
-// 2. <js>
-// 3. <script>
-// 4. <javascript>
 var countdown
 var startTimer
 var timerEl = document.getElementById('countdown-timer')
@@ -37,26 +31,59 @@ function nextQuestion(currentQuestion) {
 
 document.querySelector('#button').addEventListener('click', startQuiz)
 
-
 // var time = Questions.length * 10
 
-// var Questions = [
-//     {
-//         question: "somehting?",
-//         choices: ["a", "b", "c", "d"],
-//         answer: "b"
-//     },
-//     {
-//         question: "somehting?",
-//         choices: ["a", "b", "c", "d"],
-//         answer: "b"
-//     },
-//     {
-//         question: "somehting?",
-//         choices: ["a", "b", "c", "d"],
-//         answer: "b"
-//     }
-// ];
+var questionsArray = [
+    {
+        question: "Inside which html tag do we put the JavaScript?",
+        choices: [
+                "<scripting>",
+                "<js>", 
+                "<script>", 
+                "<javascript>"],
+        answer: "c"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store_____.",
+        choices: ["nnumbers and strings", 
+                 "other arrays", 
+                 "booleans", 
+                 "all the above"],
+        answer: "d"
+    },
+    {
+        question: "Commonly used data types do not include______. ",
+        choices: ["boleans", 
+                 "strings", 
+                 "alerts", 
+                 "numbers"],
+        answer: "c"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within______. ",
+        choices: ["quotes", 
+                 "parantheses", 
+                 "curly brackets", 
+                 "square brackets"],
+        answer: "b"
+    },
+    {
+        question: "String variables must be enclosed within ______ when being assigned to variables. ",
+        choices: ["brackets", 
+                 "quotes", 
+                 "curly brackets", 
+                 "hyphens"],
+        answer: "a"
+    },
+    {
+        question: "JavaScript was originally developed by_____. ",
+        choices: ["Bill Gates", 
+                 "MySpace", 
+                 "Netscape", 
+                 "Napster"],
+        answer: "c"
+    }
+];
 
 
 // //Button to initiate timer and quiz add event listener
