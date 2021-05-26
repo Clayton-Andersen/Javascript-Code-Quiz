829d7f808ea989c5c98d8a9f8c19d73485c4e2ab
Aloha <utah.clayton@gmail.com>
2021-05-25 20:40:58 -0700

    start quiz function

--- assets/js/code-quiz.js
+++ assets/js/code-quiz.js
@@ -1,116 +1,49 @@
+var time = 1;
+var timerId;
+var score;
+
+
+var timerEl = document.getElementById("time");
+var startScreen = document.getElementById("startScreen");
+var questionBody = document.getElementById("question-body");
+
+function timer() {
+    time--;
+    timerEl.textContent = time;
+
+    if (time===0) {
+        clearInterval(timerId);
+    };
+    if (time <= 0) {
+        // quizEnd()
+    }
+}
+
+function startQuiz() {
+    startScreen.setAttribute("class", "hide");
+    questionBody.removeAttribute("class","hide");
+
+    timerId = setInterval(timer, 1000);
+    timerEl.textContent = time;
+    
+}
 
 var countdown
 var startTimer
 var timerEl = document.getElementById('countdown-timer')
 var segments = document.getElementsByClassName('segment')
 
-function timer() {
-    countdown = 60
-    startTimer = setInterval(function () {
-        countdown--
-        console.log(countdown)
-        timerEl.innerHTML = countdown;
-        if (countdown === 0) {
-            clearInterval(startTimer);
-        }
-    }, 1000)
+
+function startQuestion() {
+    // you want to start from the first index of your questions object 
+    // to print every question with forEach() method in JS
+    // each list items need to be clickable with a function click attached
 }
 
-function startQuiz() {
-    timer();
-
-    segments[0].classList.remove("hide");
-}
-
-function nextQuestion(currentQuestion) {
-    segments[currentQuestion].classList.add("hide");
-    var followingQuestion = currentQuestion + 1;
-    segments[followingQuestion].classList.remove("hide");
-}
+// another function to evaluate your choices
 
 
-document.querySelector('#button').addEventListener('click', startQuiz)
-
-// var time = Questions.length * 10
-
-var questionsArray = [
-    {
-        question: "Inside which html tag do we put the JavaScript?",
-        choices: [
-            "<scripting>",
-            "<js>",
-            "<script>",
-            "<javascript>"],
-        answer: "c"
-    },
-    {
-        question: "Arrays in JavaScript can be used to store_____.",
-        choices: [
-            "nnumbers and strings",
-            "other arrays",
-            "booleans",
-            "all the above"],
-        answer: "d"
-    },
-    {
-        question: "Commonly used data types do not include______. ",
-        choices: [
-            "boleans",
-            "strings",
-            "alerts",
-            "numbers"],
-        answer: "c"
-    },
-    {
-        question: "The condition in an if/else statement is enclosed within______. ",
-        choices: [
-            "quotes",
-            "parantheses",
-            "curly brackets",
-            "square brackets"],
-        answer: "b"
-    },
-    {
-        question: "String variables must be enclosed within ______ when being assigned to variables. ",
-        choices: [
-            "brackets",
-            "quotes",
-            "curly brackets",
-            "hyphens"],
-        answer: "a"
-    },
-    {
-        question: "JavaScript was originally developed by_____. ",
-        choices: [
-            "Bill Gates",
-            "MySpace",
-            "Netscape",
-            "Napster"],
-        answer: "c"
-    }
-];
-for (var i = 0; i < questionsArray.length; i++) {
-    console.log(questionsArray)
-}
 
 
-// //Button to initiate timer and quiz add event listener
-// //Questions displayed and list answers
-// //Check user input right and wrong add event listener
-// //if statement if right, next question, if wrong deduct 10 sec and move to next q
-// //after iterated all questions, game ends or time expires and game ends
-// //game ends: display end screen, timer stops. Local storage of score
-// for (let i = 0; i < Questions.length; i++) {
-//     console.log(Questions[i].answer);
-//     for (let z = 0; z < Questions[i].choices.length; z++) {
-//         console.log(Questions[i].choices[z])
+document.querySelector("#start").addEventListener("click", startQuiz)
 
-//     }
-
-// }
-
-// // if (useranswer === Questions[i].answer) {
-// //     points++
-// // } else if(useranswer !== Questions.answer){
-// //     time =- 5
-// // }
---
+++ assets/js/questions.js
@@ -0,0 +1,62 @@
+var questionsArray = [
+    {
+        question: "Inside which html tag do we put the JavaScript?",
+        choices: [
+            "<scripting>",
+            "<js>",
+            "<script>",
+            "<javascript>"
+        ],
+        answer: "c"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store_____.",
+        choices: [
+            "nnumbers and strings",
+            "other arrays",
+            "booleans",
+            "all the above"
+        ],
+        answer: "d"
+    },
+    {
+        question: "Commonly used data types do not include______. ",
+        choices: [
+            "boleans",
+            "strings",
+            "alerts",
+            "numbers"
+        ],
+        answer: "c"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within______. ",
+        choices: [
+            "quotes",
+            "parantheses",
+            "curly brackets",
+            "square brackets"
+        ],
+        answer: "b"
+    },
+    {
+        question: "String variables must be enclosed within ______ when being assigned to variables. ",
+        choices: [
+            "brackets",
+            "quotes",
+            "curly brackets",
+            "hyphens"
+        ],
+        answer: "a"
+    },
+    {
+        question: "JavaScript was originally developed by_____. ",
+        choices: [
+            "Bill Gates",
+            "MySpace",
+            "Netscape",
+            "Napster"
+        ],
+        answer: "c"
+    }
+];
