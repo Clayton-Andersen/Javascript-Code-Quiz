2dc672324d990eea7b02faad965b4ccdd104642b
Aloha <utah.clayton@gmail.com>
2021-05-28 07:47:51 -0700

    more pseudo code for question anwer section

--- assets/js/code-quiz.js
+++ assets/js/code-quiz.js
@@ -1,11 +1,13 @@
 var time = 1;
 var timerId;
 var score;
+var questionIndex = 0;
 
 
 var timerEl = document.getElementById("time");
 var startScreen = document.getElementById("startScreen");
 var questionBody = document.getElementById("question-body");
+var choicesEl = document.getElementById("choices");
 
 function timer() {
     time--;
@@ -25,7 +27,7 @@ function startQuiz() {
 
     timerId = setInterval(timer, 1000);
     timerEl.textContent = time;
-    
+    startQuestion();
 }
 
 var countdown
@@ -35,6 +37,17 @@ var segments = document.getElementsByClassName('segment')
 
 
 function startQuestion() {
+    var currentQuestion = questionsArray[questionIndex];
+    var questionTitleEl = document.getElementById('questionTitle')
+    questionTitleEl.textContent = currentQuestion.question
+    choicesEl.innerHTML = ""
+    currentQuestion.choices.forEach(function (choice, i) {
+        //declare var document.createEl, set attributes (choice) set text content to each choice 
+        //and add event listener to start next question append element(btn) 
+
+        //new function to check q right or wrong and score.
+        //run start question again increment index
+    })
     // you want to start from the first index of your questions object 
     // to print every question with forEach() method in JS
     // each list items need to be clickable with a function click attached
@@ -44,6 +57,38 @@ function startQuestion() {
 
 
 
-
 document.querySelector("#start").addEventListener("click", startQuiz)
 
+// var countdown
+// var startTimer
+// var timerEl = document.getElementById('countdown-timer')
+// var segments = document.getElementsByClassName('segment')
+
+// function timer() {
+//         countdown = 60
+//         startTimer = setInterval(function(){
+//         countdown--
+//         console.log(countdown)
+//         timerEl.innerHTML = countdown;
+//         if (countdown===0){
+//             clearInterval(startTimer);
+//         }
+//     },1000)
+// }
+
+// function startQuiz() {
+//     timer();
+  
+//     segments[0].classList.remove("hide");
+//     }
+    
+// function nextQuestion(currentQuestion) {
+//     segments[currentQuestion].classList.add("hide");
+//     var followingQuestion = currentQuestion + 1;
+//     segments[followingQuestion].classList.remove("hide");
+// }
+
+
+// document.querySelector('#button').addEventListener('click', startQuiz)
+
+// // var time = Questions.length * 10
